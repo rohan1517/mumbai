82fc2e9146dd2323946ed0408cb75353238c9cf4
rohan1517 <rohanmali1@gmail.com>
2017-05-29 03:55:11 -0500

    Working file 29052017

---
+++ wp-content/themes/vision-church/Change_log.php
@@ -0,0 +1,32 @@
+<?php
+return $change_log = '
+v 1.1.0 – 10 April 2017
+- Updated : Visual composer to v5.1.1
+- Updated: Modern Event Calendar to v1.9.3.1
+- Compatibility: Woocommerce 3.0.1
+- Added: Featured image in Webnus Sermons element (Grid type)
+- Added: "Series" Taxonomy to Webnus Sermons plugin
+- Changed: Single Sermon page style
+
+v 1.0.3 – 4 April 2017
+- Updated: Modern Event Calendar to v1.9.3
+- Updated: The Grid to v2.3.5
+- Added: Smooth Scroll Option
+- Added: New Prayer Wall plugin
+- Removed: Old Prayer Wall plugin
+- Fixed: Menu Item Style
+- Fixed: Forward Demo Importer
+
+v 1.0.2 – 28 March 2017
+- Added: Thumbnail Generator for some elements and Featured Images.
+- Fixed: Some CSS
+- Fixed: Some minor issues
+
+v 1.0.1 – 27 March 2017
+- Fixed: Demo Importer
+- Fixed: Page-options issues
+- Fixed: Some minor issues
+
+v 1.0.0 – 27 March 2017
+- Initial Release
+';
